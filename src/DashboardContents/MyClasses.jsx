1205dbe54c3a2af05a56cc6325eeb08fa60f9e11
Nazmul Ahsan <xeeshann.ahsan@gmail.com>
2024-05-11 15:22:01 +0600

    delete by params

--- src/DashboardContents/MyClasses.jsx
+++ src/DashboardContents/MyClasses.jsx
@@ -4,7 +4,7 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import Swal from "sweetalert2";
 import Container from "../Components/Layouts/Container";
 const MyClasses = () => {
