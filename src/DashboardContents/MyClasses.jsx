05e54934100186a458bc88898ad7371cc7e068a6
Nazmul Ahsan <xeeshann.ahsan@gmail.com>
2024-05-11 12:41:50 +0600

    fixation

--- src/DashboardContents/MyClasses.jsx
+++ src/DashboardContents/MyClasses.jsx
@@ -43,8 +43,11 @@ const MyClasses = () => {
     if (confirmed.isConfirmed) {
       try {
         // Send delete request
-        const response = await axios.delete(
-          `https://creative-school-design.onrender.com/api/v1/classes/deleteclass/${id}`
+        const response = await axios.post(
+          `https://creative-school-design.onrender.com/api/v1/classes/deleteclass/`,
+          {
+            id,
+          }
         );
 
         // Handle success response
