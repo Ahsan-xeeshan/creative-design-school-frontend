decb8b495be492c17b822cdd64a3e140bf3dfec4
Nazmul Ahsan <xeeshann.ahsan@gmail.com>
2024-05-11 15:05:59 +0600

    another check

--- src/DashboardContents/MyClasses.jsx
+++ src/DashboardContents/MyClasses.jsx
@@ -44,7 +44,8 @@ const MyClasses = () => {
       try {
         // Send delete request
         const response = await axios.delete(
-          `https://creative-school-design.onrender.com/api/v1/classes/deleteclass/${id}`
+          `https://creative-school-design.onrender.com/api/v1/classes/deleteclass`,
+          { id }
         );
 
         // Handle success response
