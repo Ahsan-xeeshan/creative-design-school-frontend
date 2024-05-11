20810bb36194939ba31eac525cf3446a89fd6c26
Nazmul Ahsan <xeeshann.ahsan@gmail.com>
2024-05-11 17:36:11 +0600

    null check

--- src/Components/Layouts/ClassCard.jsx
+++ src/Components/Layouts/ClassCard.jsx
@@ -74,7 +74,9 @@ const ClassCard = () => {
                     <span className="text-xl"> {item.feedback}</span>
                   </p>
                 </div>
-                {data === null && data.role === "student" && (
+                {data.role === "admin" && data.role === "instructor" ? (
+                  <div className="hidden"></div>
+                ) : (
                   <div className="p-4 bg-purple-100 flex h-16 border-t items-center justify-between">
                     <button
                       className="p-3  font-bold rounded-xl bg-blue-200 text-blue-800            
